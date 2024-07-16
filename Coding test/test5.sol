2bbe400c7a1c9e96080f8443328f0b6a1a84e591
Jo-Jae <whwodjs11@gmail.com>
2024-07-16 09:28:27 +0900

    test5

---
+++ Coding test/test5.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >= 0.8.2 < 0.9.0;
+
+contract TEST5 {
+
+/*숫자를 시분초로 변환하세요.
+예) 100 -> 1 min 40 sec
+600 -> 10min 
+1000 -> 16min 40sec
+5250 -> 1hour 27min 30sec
+3600 60
+*/
+
+    function getTime(uint _n) public pure returns(uint,string memory, uint, string memory,uint, string memory){
+       uint a;
+
+        if (_n >= 3600) {}
+
+            return ;
+        } else if (_n >= 60) {
+            return (( _n / 60) ,"min", (_n % 60) ,"sec")
+        } else return (_n,"sec");
+    }
+
+}
