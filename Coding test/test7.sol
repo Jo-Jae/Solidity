cf47c73a8c166f175cca5caee427c45fdb296745
Jo-Jae <whwodjs11@gmail.com>
2024-07-23 09:54:14 +0900

    test7

---
+++ Coding test/test7.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >= 0.8.2 < 0.9.0;
+
+contract TEST6 {
+/* * 악셀 기능 - 속도를 10 올리는 기능, 악셀 기능을 이용할 때마다 연료가 20씩 줄어듬, 
+연료가 30이하면 더 이상 악셀을 이용할 수 없음, 속도가 70이상이면 악셀 기능은 더이상 못씀
+* 주유 기능 - 주유하는 기능, 주유를 하면 1eth를 지불해야하고 연료는 100이 됨
+* 브레이크 기능 - 속도를 10 줄이는 기능, 브레이크 기능을 이용할 때마다 연료가 10씩 줄어듬, 
+속도가 0이면 브레이크는 더이상 못씀
+* 시동 끄기 기능 - 시동을 끄는 기능, 속도가 0이 아니면 시동은 꺼질 수 없음
+* 시동 켜기 기능 - 시동을 켜는 기능, 시동을 키면 정지 상태로 설정됨
+--------------------------------------------------------
+* 충전식 기능 - 지불을 미리 해놓고 추후에 주유시 충전금액 차감 */
+
+    enum Status {
+        Accel,
+        Refill,
+        Brake,
+        TurnOn,
+        TurnOff
+    }
+
+
+    Status s;
+    uint fuel = 150;
+    uint speed = 0;
+
+
+    function TurnOn() public {
+        s = Status.TurnOn;
+    }
+
+    function TurnOff() public {
+        require(speed == 0, "Nope");
+        s = Status.TurnOff;
+    }
+
+    function Accel() public {
+        require(fuel > 30, "Nope");
+        require(speed < 70, "Nope");
+        s = Status.Accel;
+        fuel -= 20;
+        speed += 10;
+        
+    } 
+
+    function Brake() public {
+        require(speed != 0, "Nope");
+        s = Status.Brake;
+        fuel -= 10;
+        speed -= 10;
+    }
+
+    function Refill() public payable {
+       msg.sender 
+       s = Status.Refill;
+       fuel += 100;
+
+    }
+
+}
--- Solidity100/sole100(31~40).sol
+++ Solidity100/sole100(31~40).sol
@@ -154,7 +154,7 @@ contract Q37 {
     /*1 wei를 기부하는 기능, 1finney를 기부하는 기능 그리고 1 ether를 기부하는 기능을 구현하세요. 
     최초의 배포자만이 해당 smart contract에서 자금을 회수할 수 있고 다른 이들은 못하게 막는 함수도 구현하세요.
     (힌트 : 기부는 EOA가 해당 Smart Contract에게 돈을 보내는 행위, contract가 돈을 받는 상황)*/
-
+    function deposit() public payable {}
    
     address payable owner;
 
@@ -172,9 +172,9 @@ contract Q37 {
     }
 
     function Donation() public {
-        payable(msg.sender).transfer(1 ether);
-    }
+    msg.sender.transfer.(1 ether);
 
+    }
 
 
 
